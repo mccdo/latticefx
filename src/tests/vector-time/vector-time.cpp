da62076ecaf973497dfe67e7c3d5a448a28ca6e2
pmartz <pmartz@2dfa2521-ee2f-031c-41c9-31afc9f14f0a>
2012-05-01 22:12:36 +0000

    Larger data set for vector-time. ~400MB of sample points in this revision.

--- src/tests/vector-time/vector-time.cpp
+++ src/tests/vector-time/vector-time.cpp
@@ -2,7 +2,6 @@
 #include <latticefx/DataSet.h>
 #include <latticefx/ChannelData.h>
 #include <latticefx/ChannelDataOSGArray.h>
-#include <latticefx/RTPOperation.h>
 #include <latticefx/Renderer.h>
 #include <latticefx/PlayControl.h>
 #include <latticefx/TextureUtils.h>
@@ -20,6 +19,7 @@
 #include <osgDB/FileUtils>
 
 #include <osgwTools/Shapes.h>
+#include <iostream>
 
 
 class InstancedVectors : public lfx::Renderer
@@ -83,8 +83,11 @@ protected:
 lfx::DataSetPtr prepareDataSet()
 {
     osg::ref_ptr< osg::Vec3Array > vertArray( new osg::Vec3Array );
-    const unsigned int w( 8 ), h( 4 ), d( 1 );
-    vertArray->resize( w*h*d );
+    const unsigned int w( 73 ), h( 41 ), d( 11 );
+    std::cout << "Creating data set. Dimentions: " << w << " x " << h << " x " << d << std::endl;
+
+    unsigned int samplesPerTime( w*h*d );
+    vertArray->resize( samplesPerTime );
     unsigned int wIdx, hIdx, dIdx, index( 0 );
     for( wIdx=0; wIdx<w; ++wIdx )
     {
@@ -94,7 +97,7 @@ lfx::DataSetPtr prepareDataSet()
             {
                 const float x( ((double)wIdx)/(w-1.) * (double)w - (w*.5) );
                 const float y( ((double)hIdx)/(h-1.) * (double)h - (h*.5) );
-                const float z( 0. );
+                const float z( ((double)dIdx)/(d-1.) * (double)d - (d*.5) );
                 (*vertArray)[ index ].set( x, y, z );
                 ++index;
             }
@@ -105,8 +108,13 @@ lfx::DataSetPtr prepareDataSet()
     lfx::DataSetPtr dsp( new lfx::DataSet() );
     dsp->addChannel( vertData );
 
+    const double maxTime( 8. );
+    const double sampleRate( 60. );
+    std::cout << "Creating time series data. " << maxTime << "s, sample rate: " << sampleRate << "hz." << std::endl;
+
+    int count( 0 );
     double time;
-    for( time=0.; time<1.0; time += 0.1 )
+    for( time=0.; time<maxTime; time += 1./sampleRate )
     {
         osg::ref_ptr< osg::Vec3Array > dirArray( new osg::Vec3Array );
         dirArray->resize( w*h*d );
@@ -118,14 +126,17 @@ lfx::DataSetPtr prepareDataSet()
                 for( dIdx=0; dIdx<d; ++dIdx )
                 {
                     const float x( ((double)wIdx)/(w-1.) * (double)w - (w*.5) );
-                    (*dirArray)[ index ].set( sin( x*2. ), -sin( time ), .5 );
+                    const float y( ((double)hIdx)/(h-1.) * (double)h - (h*.5) );
+                    (*dirArray)[ index ].set( sin( x+y+time ), sin( (x+y+time)*1.2 ), .5 );
                     ++index;
+                    ++count;
                 }
             }
         }
         lfx::ChannelDataOSGArrayPtr dirData( lfx::ChannelDataOSGArrayPtr( new lfx::ChannelDataOSGArray( dirArray.get(), "directions" ) ) );
         dsp->addChannel( dirData, time );
     }
+    std::cout << "Total samples: " << count << std::endl;
 
     lfx::RendererPtr renderOp( new InstancedVectors() );
     renderOp->addInput( "positions" );
