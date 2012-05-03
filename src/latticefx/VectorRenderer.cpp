9f87abd25b1a81c78eb77d770f3b012d68adde63
pmartz <pmartz@2dfa2521-ee2f-031c-41c9-31afc9f14f0a>
2012-05-03 01:14:22 +0000

    Establishing mechanism to "alias" a name to a Renderer-specific input.

--- src/latticefx/VectorRenderer.cpp
+++ src/latticefx/VectorRenderer.cpp
@@ -14,6 +14,7 @@
 #include <osgDB/FileUtils>
 
 #include <osgwTools/Shapes.h>
+#include <string>
 
 
 
@@ -21,18 +22,27 @@ namespace lfx {
 
 
 VectorRenderer::VectorRenderer()
-  : lfx::Renderer()
+  : lfx::Renderer(),
+    _pointStyle( SIMPLE_POINTS )
 {
+    setInputNameAlias( POSITION, "positions" );
+    setInputNameAlias( DIRECTION, "directions" );
+    setInputNameAlias( RADIUS, "radii" );
 }
 VectorRenderer::VectorRenderer( const VectorRenderer& rhs )
+  : lfx::Renderer( rhs ),
+    _pointStyle( rhs._pointStyle ),
+    _inputTypeMap( rhs._inputTypeMap )
 {
 }
 VectorRenderer::~VectorRenderer()
-{}
+{
+}
 
 osg::Node* VectorRenderer::getSceneGraph( const lfx::ChannelDataPtr maskIn )
 {
-    const osg::Array* sourceArray( getInput( "positions" )->asOSGArray() );
+    const ChannelDataPtr posChannel( getInput( getInputTypeAlias( POSITION ) ) );
+    const osg::Array* sourceArray( posChannel->asOSGArray() );
     const osg::Vec3Array* positions( dynamic_cast< const osg::Vec3Array* >( sourceArray ) );
 
     osg::ref_ptr< osg::Geode > geode( new osg::Geode );
@@ -51,12 +61,13 @@ osg::Node* VectorRenderer::getSceneGraph( const lfx::ChannelDataPtr maskIn )
 
     osg::StateSet* stateSet( geode->getOrCreateStateSet() );
 
-    osg::Texture3D* posTex( lfx::createTexture3DForInstancedRenderer( getInput( "positions" ) ) );
+    osg::Texture3D* posTex( lfx::createTexture3DForInstancedRenderer( posChannel ) );
     stateSet->setTextureAttributeAndModes( 0, posTex, osg::StateAttribute::OFF );
     osg::Uniform* posUni( new osg::Uniform( osg::Uniform::SAMPLER_3D, "texPos" ) ); posUni->set( 0 );
     stateSet->addUniform( posUni );
 
-    osg::Texture3D* dirTex( lfx::createTexture3DForInstancedRenderer( getInput( "directions" ) ) );
+    const ChannelDataPtr dirChannel( getInput( getInputTypeAlias( DIRECTION ) ) );
+    osg::Texture3D* dirTex( lfx::createTexture3DForInstancedRenderer( dirChannel ) );
     stateSet->setTextureAttributeAndModes( 1, dirTex, osg::StateAttribute::OFF );
     osg::Uniform* dirUni( new osg::Uniform( osg::Uniform::SAMPLER_3D, "texDir" ) ); dirUni->set( 1 );
     stateSet->addUniform( dirUni );
@@ -75,6 +86,28 @@ osg::Node* VectorRenderer::getSceneGraph( const lfx::ChannelDataPtr maskIn )
     program->addShader( fragmentShader );
 
     return( geode.release() );
+}
+
+void VectorRenderer::setPointStyle( const PointStyle& pointStyle )
+{
+    _pointStyle = pointStyle;
+}
+VectorRenderer::PointStyle VectorRenderer::getPointStyle() const
+{
+    return( _pointStyle );
+}
+
+void VectorRenderer::setInputNameAlias( const InputType& inputType, const std::string& alias )
+{
+    _inputTypeMap[ inputType ] = alias;
+}
+std::string VectorRenderer::getInputTypeAlias( const InputType& inputType ) const
+{
+    InputTypeMap::const_iterator it( _inputTypeMap.find( inputType ) );
+    if( it != _inputTypeMap.end() )
+        return( it->second );
+    else
+        return( "" );
 }
 
 
--- src/latticefx/VectorRenderer.h
+++ src/latticefx/VectorRenderer.h
@@ -7,7 +7,8 @@
 #include <latticefx/Renderer.h>
 
 #include <boost/smart_ptr/shared_ptr.hpp>
-
+#include <string>
+#include <map>
 
 
 namespace lfx {
@@ -24,9 +25,45 @@ public:
     VectorRenderer( const VectorRenderer& rhs );
     virtual ~VectorRenderer();
 
+    /** \brief Override base class lfx::Renderer
+    \details Invoked by DataSet to obtain a scene graph for rendering points. */
     virtual osg::Node* getSceneGraph( const lfx::ChannelDataPtr maskIn );
 
+    /** \brief
+    \details */
+    typedef enum {
+        SIMPLE_POINTS,
+        POINT_SPRITES,
+        SPHERES,
+        DIRECTION_VECTORS
+    } PointStyle;
+
+    /** \brief
+    \details */
+    void setPointStyle( const PointStyle& pointStyle );
+    /** \brief
+    \details */
+    PointStyle getPointStyle() const;
+
+    /** \brief
+    \details */
+    typedef enum {
+        POSITION,
+        DIRECTION,
+        RADIUS
+    } InputType;
+    typedef std::map< InputType, std::string > InputTypeMap;
+
+    /** \brief
+    \details */
+    void setInputNameAlias( const InputType& inputType, const std::string& alias );
+    /** \brief
+    \details */
+    std::string getInputTypeAlias( const InputType& inputType ) const;
+
 protected:
+    PointStyle _pointStyle;
+    InputTypeMap _inputTypeMap;
 };
 
 typedef boost::shared_ptr< VectorRenderer > VectorRendererPtr;
