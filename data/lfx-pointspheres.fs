123f00449b8acde3ee502aad27e2b5b5c253366f
pmartz <pmartz@2dfa2521-ee2f-031c-41c9-31afc9f14f0a>
2012-05-11 21:28:24 +0000

    Fixing support for 2-sided lighting.

--- data/lfx-pointspheres.fs
+++ data/lfx-pointspheres.fs
@@ -8,13 +8,19 @@ varying vec3 ecNormal;
 
 vec4 fragmentLighting( vec4 baseColor )
 {
+    vec3 normal;
+    if( gl_FrontFacing )
+        normal = ecNormal;
+    else
+        normal = -ecNormal;
+
     vec3 lightVec = normalize( gl_LightSource[0].position.xyz - ecVertex );
     vec3 eyeVec = normalize( -ecVertex );
-    vec3 reflectVec = normalize( -reflect( lightVec, ecNormal ) );
+    vec3 reflectVec = normalize( -reflect( lightVec, normal ) );
 
     vec4 amb = gl_LightSource[0].ambient * baseColor;
 
-    vec4 diff = gl_LightSource[0].diffuse * baseColor * max( dot( ecNormal, lightVec ), 0. );
+    vec4 diff = gl_LightSource[0].diffuse * baseColor * max( dot( normal, lightVec ), 0. );
     diff = clamp( diff, 0., 1. );
    
     // Hm. front material shininess is negative for some reason. Hack in "10.0" for now.
