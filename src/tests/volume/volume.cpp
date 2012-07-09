d16203c2f7cf12f0d128d1791c9ec5dfb8adcbc4
pmartz <pmartz@2dfa2521-ee2f-031c-41c9-31afc9f14f0a>
2012-07-09 14:42:30 +0000

    Adding command line params to volume test.

--- src/tests/volume/volume.cpp
+++ src/tests/volume/volume.cpp
@@ -42,7 +42,7 @@
 
 
 
-lfx::DataSetPtr prepareVolume( const std::string& fileName )
+lfx::DataSetPtr prepareVolume( const std::string& fileName, const osg::Vec3& dims )
 {
     osg::Image* image( osgDB::readImageFile( fileName ) );
     if( image == NULL )
@@ -57,7 +57,7 @@ lfx::DataSetPtr prepareVolume( const std::string& fileName )
     dsp->addChannel( volumeData );
 
     lfx::VolumeRendererPtr renderOp( new lfx::VolumeRenderer() );
-    renderOp->setVolumeDims( osg::Vec3( 60., 60., 30. ) );
+    renderOp->setVolumeDims( dims );
     renderOp->setPlaneSpacing( .3f );
     renderOp->addInput( "volumedata" );
     dsp->setRenderer( renderOp );
@@ -81,42 +81,47 @@ int main( int argc, char** argv )
     std::string fileName( "HeadVolume.dds" );
     arguments.read( "-f", fileName );
 
+    osg::Vec3 dims( 50., 50., 50. );
+    arguments.read( " -d", dims[0],dims[1],dims[2] );
+
     // Create an example data set.
 	osg::Group* root (new osg::Group);
     root->getOrCreateStateSet()->setRenderingHint( osg::StateSet::TRANSPARENT_BIN );
 
-    osg::MatrixTransform* mtA( new osg::MatrixTransform );
-	osg::MatrixTransform* mtB( new osg::MatrixTransform );
-	osg::MatrixTransform* mtC( new osg::MatrixTransform );
-    lfx::DataSetPtr dsp( prepareVolume( fileName ) );
+    lfx::DataSetPtr dsp( prepareVolume( fileName, dims ) );
 
-	osg::Vec3 volumeDimensions(60.0f, 60.0f, 30.0f), volumePosition(0.0f, 0.0f, 0.0f);
-	// unclear how to set these dimensions from here
-
-	// Test Matrix position/scale
-	osg::Matrix transformA, transformB, transformC;
-	// the translate will occur in the unscaled units, and the scaling will occur around the new origin.
-	// A is just translate and nominal scale
-	transformA *= osg::Matrixd::scale(1.0, 1.0, 0.5);
-	transformA *= osg::Matrixd::translate(-50.0, 0.0, 0.0);
-	mtA->setMatrix(transformA);
-	// B: scale and rotate but no translate
-	transformB *= osg::Matrixd::rotate(osg::DegreesToRadians(45.0), 0.0, 1.0, 0.0); // 45 degrees about +Y axis
-	transformB *= osg::Matrixd::scale(2.0, 2.0, 2.0);
-	transformB *= osg::Matrixd::translate(0.0, 0.0, 0.0);
-	mtB->setMatrix(transformB);
-	// C: translate, scale AND rotate
-	transformC *= osg::Matrixd::rotate(osg::DegreesToRadians(45.0), 1.0, 0.0, 0.0); // 45 degrees about +X axis
-	transformC *= osg::Matrixd::scale(2.0, 2.0, 1.0);
-	transformC *= osg::Matrixd::translate(50.0, 0.0, 0.0);
-	mtC->setMatrix(transformC);
-	root->addChild(mtA);
-	root->addChild(mtB);
-	root->addChild(mtC);
-	// multi-parent the same scene data
-	mtA->addChild( dsp->getSceneData() );
-	mtB->addChild( dsp->getSceneData() );
-	mtC->addChild( dsp->getSceneData() );
+    if( arguments.find( "-mt" ) > 0 )
+    {
+        osg::MatrixTransform* mtA( new osg::MatrixTransform );
+	    osg::MatrixTransform* mtB( new osg::MatrixTransform );
+	    osg::MatrixTransform* mtC( new osg::MatrixTransform );
+	    // Test Matrix position/scale
+	    osg::Matrix transformA, transformB, transformC;
+	    // the translate will occur in the unscaled units, and the scaling will occur around the new origin.
+	    // A is just translate and nominal scale
+	    transformA *= osg::Matrixd::scale(1.0, 1.0, 0.5);
+	    transformA *= osg::Matrixd::translate(-50.0, 0.0, 0.0);
+	    mtA->setMatrix(transformA);
+	    // B: scale and rotate but no translate
+	    transformB *= osg::Matrixd::rotate(osg::DegreesToRadians(45.0), 0.0, 1.0, 0.0); // 45 degrees about +Y axis
+	    transformB *= osg::Matrixd::scale(2.0, 2.0, 2.0);
+	    transformB *= osg::Matrixd::translate(0.0, 0.0, 0.0);
+	    mtB->setMatrix(transformB);
+	    // C: translate, scale AND rotate
+	    transformC *= osg::Matrixd::rotate(osg::DegreesToRadians(45.0), 1.0, 0.0, 0.0); // 45 degrees about +X axis
+	    transformC *= osg::Matrixd::scale(2.0, 2.0, 1.0);
+	    transformC *= osg::Matrixd::translate(50.0, 0.0, 0.0);
+	    mtC->setMatrix(transformC);
+	    root->addChild(mtA);
+	    root->addChild(mtB);
+	    root->addChild(mtC);
+	    // multi-parent the same scene data
+	    mtA->addChild( dsp->getSceneData() );
+	    mtB->addChild( dsp->getSceneData() );
+	    mtC->addChild( dsp->getSceneData() );
+    }
+    else
+        root->addChild( dsp->getSceneData() );
 
     /*
     // Test hardware clip planes
