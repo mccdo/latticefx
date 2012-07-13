d44178b86cd0d1b3171e59d63df26485971fd8d1
pmartz <pmartz@2dfa2521-ee2f-031c-41c9-31afc9f14f0a>
2012-07-13 16:56:48 +0000

    Doxygen comments for the SurfaceRenderer.

--- src/latticefx/core/SurfaceRenderer.h
+++ src/latticefx/core/SurfaceRenderer.h
@@ -46,15 +46,31 @@ namespace lfx {
 
 /** \class PrimitiveSetGenerator SurfaceRenderer.h <latticefx/core/SurfaceRenderer.h>
 \brief Creates OSG PrimitiveSets for the SurfaceRenderer.
-\details TBD.
+\details The SurfaceRenderer requires an instance of this class to create OSG
+Geometry PrimitiveSets.
+
+Derive from this class and override the function call operator to implement
+your own PrimitiveSet generation code. The function call operator is
+responsible for adding the PrimitiveSets to the \c geom Geometry object.
 */
 class LATTICEFX_EXPORT PrimitiveSetGenerator
 {
 public:
+    /** Constructor */
     PrimitiveSetGenerator();
+    /** Copy constructor */
     PrimitiveSetGenerator( const PrimitiveSetGenerator& rhs );
+    /** Destructor */
     virtual ~PrimitiveSetGenerator();
 
+    /** \brief Creates and attaches PrimitiveSets to the specified \c geom.
+    \details Derived classed must override this function, generate PrimitiveSets
+    to render the \c geom Geometry object, and attach those PrimitiveSets to
+    \c geom. The specified Geometry is configured with all array data (vertices,
+    normals, tex coords, vertex attribs, etc) prior to this function call
+    operator invocation.
+    \param numElements is the number of vertecies in the \c geom vertex array.
+    */
     virtual void operator()( osg::Geometry* geom, unsigned int numElements ) = 0;
 };
 
@@ -64,7 +80,30 @@ typedef boost::shared_ptr< PrimitiveSetGenerator > PrimitiveSetGeneratorPtr;
 
 /** \class SurfaceRenderer SurfaceRenderer.h <latticefx/core/SurfaceRenderer.h>
 \brief Renders arbitrary surface geometry with optional vertex warping.
-\details TBD.
+\details This class supports rendering arbitrary surface geometry. By attaching
+optional warp vector arrays, the application can render the geometry with
+hardware vertex warping.
+
+There are many ways to assemble raw vertices into an OSG Geometry for rendering.
+To support any such method, SurfaceRenderer requires an instance of
+PrimitiveSetGenerator to create and attach PrimitiveSets to the Geometry. If
+no PrimitiveSetGenerator is attached, SurfaceRenderer creates and uses an
+instance of SimpleTrianglePrimitiveSetGenerator.
+
+The name "SurfaceRenderer" is really a misnomer, as the customiability of the
+PrimitiveSetGenerator allows for vertices to be rendered as lines and points,
+not just polygons.
+
+Warp vector arrays should be added as ChannelData. Warp data for both vertices
+and normals is required; if either are missing, vertex warping is disabled.
+Warp vectors point from the source value to the "fully warped" value, and could
+be created by subtracting the fully warped values from the source values.
+
+During rendering the amount of warping is controlled with the warpScale uniform
+variable, ranging from 0.0 (no warping) to 1.0 (fully warped). Values aren't
+clamped, which supported exaggerated warping.
+
+Warping can be toggled on/off with the warpEnabled uniform variable.
 */
 class LATTICEFX_EXPORT SurfaceRenderer : public Renderer
 {
@@ -76,11 +115,11 @@ public:
     virtual osg::Node* getSceneGraph( const lfx::ChannelDataPtr maskIn );
     virtual osg::StateSet* getRootState();
 
-    /** \brief TBD
-    \details TBD */
+    /** \brief Specify the PrimitiveSetGenerator.
+    \details The default is NULL, in which case SurfaceRenderer creates and
+    uses an instance of SimpleTrianglePrimitiveSetGenerator. */
     void setPrimitiveSetGenerator( PrimitiveSetGeneratorPtr primitiveSetGenerator );
-    /** \brief TBD
-    \details TBD */
+    /** \brief Get the attached PrimitiveSetGenerator. */
     PrimitiveSetGeneratorPtr getPrimitiveSetGenerator();
 
     /** \brief Enum for input data type. */
@@ -109,6 +148,18 @@ typedef boost::shared_ptr< SurfaceRenderer > SurfaceRendererPtr;
 
 
 
+/** \class SimpleTrianglePrimitiveSetGenerator SurfaceRenderer.h <latticefx/core/SurfaceRenderer.h>
+\brief Creates TRIANGLES PrimitiveSets for the SurfaceRenderer.
+\details Interprets the \c geom vertex array as vertices suitable for rendering
+as GL_TRIANGLES. In other words, vertices n, n+1, and n+2 constiture a triangle,
+with no vertex sharing between triangles.
+
+The function call operator simply creates an osg::DrawArrays PrimitiveSet to
+render all the vertices, and attaches it to the \c geom.
+
+SurfaceRenderer creates and uses an instance of this PrimitiveSetGenerator
+by default.
+*/
 class LATTICEFX_EXPORT SimpleTrianglePrimitiveSetGenerator : public PrimitiveSetGenerator
 {
 public:
