4ae6c9c204c46c8cb176507d7393da7a4ab3c6ef
mccdo <mccdo@2dfa2521-ee2f-031c-41c9-31afc9f14f0a>
2012-08-26 16:41:47 +0000

    correct error due to reference by temporary.

--- src/tests/page2/page2.cpp
+++ src/tests/page2/page2.cpp
@@ -279,7 +279,8 @@ void assemble()
         }
     }
 
-    traverseHeirarchy( ah.getRoot(), MyHierarchyDB() );
+    MyHierarchyDB myDB;
+    traverseHeirarchy( ah.getRoot(), myDB );
     LFX_CRITICAL_STATIC( "lfx.demo", "---------------------------" );
 }
 
@@ -308,7 +309,8 @@ int main( int argc, char** argv )
     tbm->home( 0. );
 
     // Test hierarchy utils
-    traverseHeirarchy( dsp->getChannel( "texture" ), MyHierarchyDB() );
+    MyHierarchyDB myDB;
+    traverseHeirarchy( dsp->getChannel( "texture" ), myDB );
 
     // Really we would need to change the projection matrix and viewport
     // in an event handler that catches window size changes. We're cheating.
