0337d7e2b013254ae13efb1b1887bdb15df6e2bd
SkewMatrix <SkewMatrix@2dfa2521-ee2f-031c-41c9-31afc9f14f0a>
2013-02-25 22:22:54 +0000

    Minor code cleanup.

--- src/latticefx/core/VolumeRenderer.cpp
+++ src/latticefx/core/VolumeRenderer.cpp
@@ -411,13 +411,14 @@ osg::Texture3D* VolumeRenderer::createStubTexture( const DBKey& key )
     tex->setBorderWidth( 0 );
 #if 0
     // TBD why doesn't this work?
+    tex->setMaxAnisotropy( 16.f );
     tex->setFilter( osg::Texture::MIN_FILTER, osg::Texture::LINEAR_MIPMAP_LINEAR );
     tex->setFilter( osg::Texture::MAG_FILTER, osg::Texture::LINEAR );
-    tex->setMaxAnisotropy( 16.f );
 #else
     tex->setFilter( osg::Texture::MIN_FILTER, osg::Texture::NEAREST );
     tex->setFilter( osg::Texture::MAG_FILTER, osg::Texture::NEAREST );
 #endif
+
     osg::Image* dummyImage( new osg::Image );
     dummyImage->setFileName( key );
     tex->setImage( dummyImage );
