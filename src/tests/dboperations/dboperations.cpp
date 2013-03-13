043022b2d0480455a25cd1859e98c7dfb300f311
SkewMatrix <SkewMatrix@2dfa2521-ee2f-031c-41c9-31afc9f14f0a>
2013-03-13 22:12:58 +0000

    Test for DBSave RTPOperation.

--- src/tests/dboperations/dboperations.cpp
+++ src/tests/dboperations/dboperations.cpp
@@ -40,6 +40,35 @@ static std::string logstr( "lfx.ctest.dbops" );
 using namespace lfx::core;
 
 
+osg::FloatArray* genFloatArray( const int n )
+{
+    osg::ref_ptr< osg::FloatArray > floatsOrig( new osg::FloatArray() );
+    for( unsigned int idx = 0; idx < 100; ++idx )
+    {
+        floatsOrig->push_back( ( float )idx );
+    }
+    return( floatsOrig.release() );
+}
+
+bool arrayCompare( const osg::FloatArray* a, const osg::FloatArray* b )
+{
+    if( a->size() != b->size() )
+    {
+        LFX_CRITICAL_STATIC( logstr, "osg::FloatArray size mismatch." );
+        return( false );
+    }
+    for( unsigned int idx = 0; idx < a->size(); ++idx )
+    {
+        if( ( *a )[ idx ] != ( *b )[ idx ] )
+        {
+            LFX_CRITICAL_STATIC( logstr, "osg::FloatArray element mismatch." );
+            return( false );
+        }
+    }
+    return( true );
+}
+
+
 bool performTests( DBBasePtr db )
 {
     const std::string keySuffix( ( db->getImplementationType() == DBBase::DISK ) ? ".ive" : "" );
@@ -48,11 +77,7 @@ bool performTests( DBBasePtr db )
         LFX_CRITICAL_STATIC( logstr, "Testing simple array save/load to DB." );
 
         const DBKey localKey( "floats-test" + keySuffix );
-        osg::ref_ptr< osg::FloatArray > floatsOrig( new osg::FloatArray() );
-        for( unsigned int idx = 0; idx < 100; ++idx )
-        {
-            floatsOrig->push_back( ( float )idx );
-        }
+        osg::ref_ptr< osg::FloatArray > floatsOrig( genFloatArray( 100 ) );
         bool result = db->storeArray( floatsOrig.get(), localKey );
         if( !result )
         {
@@ -68,23 +93,44 @@ bool performTests( DBBasePtr db )
         }
         osg::ref_ptr< osg::FloatArray > floatsLoad( static_cast< osg::FloatArray* >( newArray ) );
 
-        if( floatsOrig->size() != floatsLoad->size() )
-        {
-            LFX_CRITICAL_STATIC( logstr, "osg::FloatArray size mismatch." );
+        bool pass( arrayCompare( floatsOrig.get(), floatsLoad.get() ) );
+        if( !pass )
             return( false );
-        }
-        for( unsigned int idx = 0; idx < floatsOrig->size(); ++idx )
-        {
-            if( ( *floatsOrig )[ idx ] != ( *floatsLoad )[ idx ] )
-            {
-                LFX_CRITICAL_STATIC( logstr, "osg::FloatArray element mismatch." );
-                return( false );
-            }
-        }
     }
 
     {
-        LFX_CRITICAL_STATIC( logstr, "Testing TEST2." );
+        LFX_CRITICAL_STATIC( logstr, "Testing DBSave FloatArray ChannelData." );
+
+
+        // Stick the array in a ChannelData. Set up a DataSet pipe with a DBSave
+        // operation to write the ChannelData to DB.
+        osg::ref_ptr< osg::FloatArray > floatsOrig( genFloatArray( 100 ) );
+        ChannelDataOSGArrayPtr cdArray( new ChannelDataOSGArray( "floatChannel", floatsOrig.get() ) );
+        const DBKey localKey( "floats-dbsave-test" + keySuffix );
+        cdArray->setDBKey( localKey );
+
+        DataSetPtr dsp( new DataSet() );
+        dsp->addChannel( cdArray );
+
+        DBSavePtr dbSave( new DBSave( db ) );
+        dbSave->addInput( "floatChannel" );
+        dsp->addOperation( dbSave );
+
+        dsp->updateAll();
+
+
+        // Do a simple retrieve from the DB and do a match test.
+        osg::Array* newArray( db->loadArray( localKey ) );
+        if( newArray == NULL )
+        {
+            LFX_CRITICAL_STATIC( logstr, "Loaded osg::FloatArray is NULL." );
+            return( false );
+        }
+        osg::ref_ptr< osg::FloatArray > floatsLoad( static_cast< osg::FloatArray* >( newArray ) );
+
+        bool pass( arrayCompare( floatsOrig.get(), floatsLoad.get() ) );
+        if( !pass )
+            return( false );
     }
 
 
