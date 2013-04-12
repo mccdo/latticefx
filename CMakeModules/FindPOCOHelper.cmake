6ab137da594661a1f12c2f22905b3d6c8142f91d
mccdo@iastate.edu <mccdo@iastate.edu@2dfa2521-ee2f-031c-41c9-31afc9f14f0a>
2013-04-12 19:26:18 +0000

    add the necessary libraries for poco.

--- CMakeModules/FindPOCOHelper.cmake
+++ CMakeModules/FindPOCOHelper.cmake
@@ -144,7 +144,7 @@ macro( POCOFinder )
         ${POCOBuildRoot} )
 
 
-    find_package( POCO 1.4.0 REQUIRED Util )
+    find_package( POCO 1.4.0 REQUIRED Foundation Util XML )
 
 
     # Restore internal variables
