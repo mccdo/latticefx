cb2acf54f56e2bfc15b79808b193c71b29982b15
SkewMatrix <SkewMatrix@2dfa2521-ee2f-031c-41c9-31afc9f14f0a>
2013-12-05 16:33:32 +0000

    Code comments and cleanup.

--- src/latticefx/core/StreamlineRenderer.h
+++ src/latticefx/core/StreamlineRenderer.h
@@ -49,61 +49,36 @@ namespace core
 
 
 /** \class StreamlineRenderer StreamlineRenderer.h <latticefx/core/StreamlineRenderer.h>
-\brief Renders vector data in a variety of styles from ChannelDataOSGArray inputs.
-\details Attach the VectorRenderer to a DataSet as follows:
+\brief Renders vector data as an animated streamline.
 
 \code
     DataSetPtr dsp( new DataSet() );
-    VectorRendererPtr renderOp( new VectorRenderer() );
+    StreamlineRendererPtr renderOp( new StreamlineRenderer() );
     dsp->setRenderer( renderOp );
 \endcode
-
-Supported rendering style:
-- SIMPLE_POINTS
-- POINT_SPRITES (not currently implemented)
-- SPHERES
-- DIRECTION_VECTORS
-
-SIMPLE_POINTS renders an array of vertex data using the GL_POINTS primitive mode.
-SPHERES renders a sphere at each position location using instanced rendering.
-DIRECTION_VECTORS renders a vector arrow at each position location using instanced rendering.
 
 Required ChannelData inputs:
 <table border="0">
   <tr>
     <td><b>InputType enum</b></td>
     <td><b>ChannelData data type</b></td>
-    <td><b>Required by</b></td>
     <td><b>Notes</b></td>
   </tr>
   <tr>
     <td>POSITION</td>
     <td>ChannelDataOSGArray (osg::Vec3Array)</td>
-    <td>All rendering syles</td>
     <td>xyz position data.</td>
-  </tr>
-  <tr>
-    <td>DIRECTION</td>
-    <td>ChannelDataOSGArray (osg::Vec3Array)</td>
-    <td>DIRECTION_VECTORS</td>
-    <td>Length determines scale factor.</td>
-  </tr>
-  <tr>
-    <td>RADIUS</td>
-    <td>ChannelDataOSGArray (osg::FloatArray)</td>
-    <td>POINT_SPRITES, SPHERES</td>
-    <td>WC radius of the sprite or sphere.</td>
   </tr>
 </table>
 
 Required name-value pair inputs (see OperationBase): None.
 
-VectorRenderer stores point and related rendering data in a specially
+StreamlineRenderer stores point and related rendering data in a specially
 formatted 3D texture data block, and extracts that data at runtime using
 the OpenGL instanced rendering feature.
 
 If the DB is non-NULL (see OperationBase::setDB() ), the 3D texture
-instanced rendering data is store in a DB during execution of
+instanced rendering data is stored in a DB during execution of
 getSceneGraph(), and paged in as-needed at runtime by the PagingCallback.
 If the DB is NULL, the 3D texture instanced rendering data is kept
 directly in the scene graph, and no paging occurs at tuntime. */
@@ -122,12 +97,6 @@ public:
     void setNumTraces( const int numTraces );
     /** \brief Get the number of streamline traces. */
     int getNumTraces() const;
-    /** \brief Specify the streamline animation speed.
-    \details The number of seconds for a trace to iterate over all vector data.
-    Smaller means faster. Default is 5.0 seconds. */
-    void setTraceInterval( const float traceInterval );
-    /** \brief Get the streamline animation speed. */
-    float getTraceInterval() const;
     /** \brief Set trace length as a percent of total data.
     \details This is a normalized percent and will be clamped to the range (0.0 - 1.0).
     Default is 0.25, or 1/4 the data length. */
